6516c540af8e67eada16e26f33660f89cbc0e33b
otera05 <otera05@gmail.com>
2018-12-18 14:35:23 +0900

    初めてのLaravel5.6：（22）HELPER 関数の追加

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -15,9 +15,9 @@
         <a href="{{ action('ArticlesController@edit', [$article->id]) }}" class="btn btn-primary">
             編集
         </a>
-        {!! Form::open(['method' => 'DELETE', 'url' => ['articles', $article->id], 'class' => 'd-inline']) !!}
-            {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
-        {!! Form::close() !!}
+
+        {!! delete_form(['articles', $article->id]) !!}
+
         <a href="{{ action('ArticlesController@index') }}" class="btn btn-secondary float-right">
             一覧へ戻る
         </a>
