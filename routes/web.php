df9dbdc88dd5b28c7e5ecf11f343aa077eae0fac
otera05 <otera05@gmail.com>
2018-12-18 16:20:01 +0900

    初めてのLARAVEL 5.6 : (25) RESOUCE CONTROLLER

--- routes/web.php
+++ routes/web.php
@@ -37,10 +37,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('articles', 'ArticlesController@index')->name('articles.index');
-Route::get('articles/create', 'ArticlesController@create')->name('articles.create');
-Route::get('articles/{id}', 'ArticlesController@show')->name('articles.show');
-Route::post('articles', 'ArticlesController@store')->name('articles.store');
-Route::get('articles/{id}/edit', 'ArticlesController@edit')->name('articles.edit');
-Route::patch('articles/{id}', 'ArticlesController@update')->name('articles.update');
-Route::delete('articles/{id}', 'ArticlesController@destroy')->name('articles.destroy');
+// Route::get('articles', 'ArticlesController@index')->name('articles.index');
+// Route::get('articles/create', 'ArticlesController@create')->name('articles.create');
+// Route::get('articles/{id}', 'ArticlesController@show')->name('articles.show');
+// Route::post('articles', 'ArticlesController@store')->name('articles.store');
+// Route::get('articles/{id}/edit', 'ArticlesController@edit')->name('articles.edit');
+// Route::patch('articles/{id}', 'ArticlesController@update')->name('articles.update');
+// Route::delete('articles/{id}', 'ArticlesController@destroy')->name('articles.destroy');
+
+Route::resource('articles', 'ArticlesController');
